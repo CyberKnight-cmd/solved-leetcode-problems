2ada6105b3fc263dd3103610f579946752ebee63
Arya Gupta <aryagupta164@gmail.com>
2025-06-30 23:46:50 +0530

    Problem Name : 0189-rotate-array Time Complexity : 231 ms (2.79%) Space Complexity : 29.4 MB (97.04%) Solved on : 06-30-2025 Solved in : .cpp

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-    void rotate(vector<int>& arr, int k) {
-        int n = arr.size();
-        k = k%n;
-        vector<int> temp(n);
-        
-        for(int i = 0; i < n; i++){
-            temp[(i+k)%n] = arr[i];
-        }
-
-        for(int i = 0; i<n;i++){
-            arr[i] = temp[i];
-            cout << arr[i] << " ";
-        }
+void rotate(vector<int> &arr, int k){
+    int n = arr.size();
+    k %= n;
+    reverse(arr.data()+(n-k), arr.data() + n);
+    reverse(arr.data(), arr.data()+(n-k));
+    reverse(arr.data(), arr.data()+n);
+    for(int i = 0; i<n;i++){
+        cout << arr[i] << " ";
     }
+}
 };
