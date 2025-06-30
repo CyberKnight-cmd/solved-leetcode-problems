5eca54eecad66d0410d7728a1b9830a607e3ae73
Arya Gupta <aryagupta164@gmail.com>
2025-07-01 00:26:51 +0530

    Problem Name : 0283-move-zeroes Time Complexity : 3 ms (18.01%) Space Complexity : 23.9 MB (18.01%) Solved on : 07-01-2025 Solved in : .cpp

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,15 +2,9 @@ class Solution {
 public:
     void moveZeroes(vector<int> &nums){
         // Searching for first 0
-        int i = 0, n = nums.size();
-        for(i = 0;i < n;i++)
-            if(!nums[i])
-                break;
-        if(i==n)    return;
-        // Starting off from the next 0
-        for(int j = i + 1; j<n; j++){
-            if(nums[j])
-                swap(nums[i++], nums[j]);
+        int j = 0, n = nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i] != 0) swap(nums[i], nums[j++]);
         }
     }
 };
